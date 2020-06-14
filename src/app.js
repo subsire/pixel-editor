4ff3e041b9cd525ba14f7139dd7f9e89570891c1
Luca Iacobini <luca@binario.it>
2020-06-14 13:19:46 +0200

    Added download button

--- src/app.js
+++ src/app.js
@@ -1,22 +1,25 @@
 import { createStore } from './store';
 
+import { Download } from './download';
 import { Form } from './form';
 import { Grid } from './grid';
 import { Picker } from './picker';
 
 const defaultState = {
-	color: '#000000',
+	color: '#ff0000',
 	size: 16,
 };
 
 export const App = () => {
 	const store = createStore(defaultState);
 
-	const form = new Form({ containerId: 'form', store });
-	const grid = new Grid({ containerId: 'grid', store });
-	const picker = new Picker({ containerId: 'picker', store });
+	const download = new Download({ containerId: 'download', store });
+	const form     = new Form({ containerId: 'form', store });
+	const grid     = new Grid({ containerId: 'grid', store });
+	const picker   = new Picker({ containerId: 'picker', store });
 
 	// Initial render
+	download.render();
 	form.render();
 	grid.render();
 	picker.render();
